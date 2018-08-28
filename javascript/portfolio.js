491694ae1097be5c8bc635eb85af666cff729d5c
NasibNia <nasibnia@gmail.com>
2018-08-28 03:25:53 -0700

    Projects are done

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -21,11 +21,11 @@ function Project(name, tech , description, imgName , webUrl, gitHub){
                                 '<div class="back">' +
                                     '<div class = "row"> <div class="col-md-12 "><p class="lead">' + this.description + '</p></div>' +
                                     '</div>' + 
-                                    '<div class = "row"> <div class="col-md-6 ">' +
-                                        '<a href=" '+ this.gitHub +  ' "target = "_blank" class="icon"><img class="img-fluid " src="assets/images/github2.png">   </a> '+ 
+                                    '<div class = "row"> <div class="col-md-6 lnk">' +
+                                        '<a href=" '+ this.gitHub +  ' "target = "_blank" "><img class="img-fluid icon-github" src="assets/images/github2.png">   </a> '+ 
                                         '</div>' +
-                                        '<div class="col-md-6 ">' +
-                                        '<a href=" '+ this.webUrl +  ' " target = "_blank" class="icon"><img class="img-fluid " src="assets/images/web.png">   </a> '+ 
+                                        '<div class="col-md-6 lnk">' +
+                                        '<a href=" '+ this.webUrl +  ' " target = "_blank" "><img class="img-fluid icon-web" src="assets/images/web.png">   </a> '+ 
                                         '</div>' +
                                     '</div>' +
                                     
@@ -52,18 +52,18 @@ $('#portfolio-id').append(this.card);
 };
 
 var proj1 = new Project ("Basic Portfolio" , ["html5","css3", "Bootstrap", "Responsiveness"], "the very first project", "IMG_1659.JPG", "https://nasibnia.github.io/Bootstrap-Portfolio/" , "https://github.com/NasibNia/Bootstrap-Portfolio");
-var proj2 = new Project ("Psychic Game Land");
-var proj3 = new Project ("Super Hero Battle Land");
-var proj4 = new Project ("Crystal Game Land");
-var proj5 = new Project ("Emothional Gify Land");
-var proj6 = new Project ("Trivia Game Land");
-var proj7 = new Project ("Train Shceduler");
-var proj8 = new Project ("Rock Paper Scissor");
-var proj9 = new Project ("Lyri Quiz");
-var proj10= new Project ("Liri Node App");
+var proj2 = new Project ("Psychic Game Land", ["html5","css3", "Bootstrap", "javascript" , "jQuery"], "very first steps into javascript","IMG_1663.JPG", "https://nasibnia.github.io/Psychic-game/.", "https://github.com/NasibNia/Psychic-game");
+var proj3 = new Project ("Super Hero Battle Land",["html5","css3", "Bootstrap", "javascript" , "jQuery"] , "learning more about javascript & jQuary", "IMG_1662.JPG", "https://nasibnia.github.io/SuperHeros/.", "https://github.com/NasibNia/SuperHeros");
+var proj4 = new Project ("Crystal Game Land",["html5","css3", "Bootstrap", "javascript" , "jQuery"],"learning more about javascript & jQuary", "crystal.jpeg", "https://nasibnia.github.io/CrystalGame/.", "https://github.com/NasibNia/CrystalGame");
+var proj5 = new Project ("Emothional Gify Land",["html5","css3", "Bootstrap", "javascript" , "jQuery" ,"API"] , "Getting to work with APIs" , "image2.gif","https://nasibnia.github.io/GifTastic/.","https://github.com/NasibNia/GifTastic");
+var proj6 = new Project ("Trivia Game Land",["html5","css3", "Bootstrap", "javascript" , "jQuery" ], "Setting Time Intervals", "animal.jpg","https://nasibnia.github.io/TriviaGame/.","https://github.com/NasibNia/TriviaGame");
+var proj7 = new Project ("Train Shceduler",["html5","css3", "Bootstrap", "javascript" , "jQuery" ,"API", "firebase", "moment.js"], "Getting to know firebase and moment.js" ,"wagon.jpg", "https://nasibnia.github.io/trainActivity/.","https://github.com/NasibNia/trainActivity");
+// var proj8 = new Project ("Rock Paper Scissor");
+var proj9 = new Project ("Lyri Quiz",["html5","css3", "Bootstrap", "javascript" , "jQuery" ,"API", "firebase", "moment.js", "buble.js", "alphabet.js", "UIKit"], "An interactive realtime game", "win.jpg", "https://jsutliff.github.io/LyriQuiz/","https://github.com/NasibNia/LyriQuiz");
+var proj10= new Project ("Liri Node App",["javascript" ,"node.js","API", "fs","inquirer","moment.js"],"Gettig to know Node.js", "bot.jpg","","https://github.com/NasibNia/Liri-Node-App");
 
 
-var projArr = [proj1,proj2,proj3,proj4,proj5,proj6,proj7,proj8,proj9,proj10];
+var projArr = [proj1,proj2,proj3,proj4,proj5,proj6,proj7,proj9,proj10];
 
 function makeCards(){
     for(var i = 0; i < projArr.length ; i++){        
@@ -79,14 +79,17 @@ $(document).ready(function () {
 
     $(document).on('click', '#portfolio', function(){
         $('#home').hide();
+        $('#about').hide();
         makeCards();
     });
 
-    // $(document).on('mouseover', '.card',function() {
-    //     console.log('hovering');
-    //     $(this).toggleClass('fron').toggleClass('back');
-    //     console.log(this);
-    // });
+    $(document).on('click', '#about-me', function(){
+        $('#home').hide();
+        $('#portfolio').hide();
+        
+    });
+
+
 
 
 
